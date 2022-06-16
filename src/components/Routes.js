c300af9ce46a209e49134b2a01b2db2cd70b7a10
Claire List <claire.list2501@gmail.com>
2022-06-15 22:52:32 -0600

    'WIP'

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,1 +1,9 @@
 //I contain the ROUTING logic of the application as a whole
+//REMEMBER TO WRAP OUR APP IN REACT ROUTER
+function Routing () {
+    return(
+        <div></div>
+    )
+}
+
+export default Routing;
