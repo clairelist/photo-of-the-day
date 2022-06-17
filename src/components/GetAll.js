5ebcdfc133543b816091d7f0a61b16e69ebae90d
Claire List <claire.list2501@gmail.com>
2022-06-17 16:12:55 -0600

    'WIP'

--- src/components/GetAll.js
+++ src/components/GetAll.js
@@ -13,20 +13,26 @@ function GetAll(){
 
     const [photos, setPhotos] = useState(loadingState);
 
+    //start date:: 1995-06-16 ...we can only return a MAX of 100 images. Waiting...
     useEffect(()=>{
-        axios.get('url')
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start-date=1995-06-16`)
         .then(res=>{
-            setPhotos(res.data);
+            setPhotos([...photos, ...res.data]); //I need to be spread in an array ! let's see what happens if we don't include ...photos here !
         }).catch(err=>{
             console.error(err);
         })
-    },[])
+    },[]) //eslint-disable-line
 
     //...we wiull have to MAP over all our images and display them below!
 
     return(
         <div>
-            <Img src={''} alt={'NASA APOD'}></Img>
+            {photos.map(photo => (
+            <div className='photos-wrapper' key={photo.date}>
+               <Img src ={photo.url} alt={`Nasa APOD for ${photo.date}`}/>
+            </div>
+            ) 
+            )}
         </div>
     )
 }
@@ -34,5 +40,6 @@ function GetAll(){
 export default GetAll;
 
 const Img = styled.img`
-height: 100%;
+height: 15%;
+width: 15%
 `
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,13 @@
 import {Route, Routes} from 'react-router-dom';
 //don't forget to import a real element to pass to our element prop!
 import GetToday from './GetToday.js';
+import GetAll from './GetAll.js';
 
 function Routing () {
     return(
         <Routes>
             <Route exact path='/' element={<GetToday />} />
+            <Route exact path='/getall' element={<GetAll />} />
         </Routes>
     )
 }
