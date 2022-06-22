83ca536150b2e7d03a2171a42786b58e6060f677
Claire List <claire.list2501@gmail.com>
2022-06-22 11:32:26 -0600

    'WIP'

--- src/components/GetRandom.js
+++ src/components/GetRandom.js
@@ -1,1 +1,39 @@
-//...I get a RANDOM image from epoch to present day !
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {API_KEY} from '../api/keys';
+
+import styled from 'styled-components';
+
+const loadingState = 'Fetching your photo. Waiting...';
+
+function GetRandom(){
+    
+    const [photo, setPhoto] = useState(loadingState);
+
+    useEffect(()=>{
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1`)
+            .then(res=>{
+                const photoObj = res.data.url;
+                setPhoto(photoObj);
+            }).catch(err=>{
+                console.error(err);
+            });
+    },[]);
+
+    return(
+        <div className='today-photo' >
+            <Img src={photo} 
+            alt={`RANDOM APOD`}
+            >
+
+            </Img>
+        </div>
+    )
+}
+
+export default GetRandom;
+
+const Img = styled.img`
+height: 100%;
+width: 100%;
+`
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,12 +2,14 @@ import {Route, Routes} from 'react-router-dom';
 //don't forget to import a real element to pass to our element prop!
 import GetToday from './GetToday.js';
 import GetAll from './GetAll.js';
+import GetRandom from './GetRandom.js';
 
 function Routing () {
     return(
         <Routes>
             <Route exact path='/' element={<GetToday />} />
             <Route exact path='/getall' element={<GetAll />} />
+            <Route exact path='/getrand' element={<GetRandom />}/>
         </Routes>
     )
 }
